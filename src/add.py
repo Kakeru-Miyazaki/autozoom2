324bc72037057cbb74820606c288d064afd7e9bc
kakeru macbook <kakekakemiya920@g.ecc.u-tokyo.ac.jp>
2021-10-30 18:08:55 +0900

    modified row of del.py for mac

--- src/add.py
+++ src/add.py
@@ -5,58 +5,55 @@ root.title('AUTOZOOM2 --add URL--')
 
 
 class dataset:
-    def __init__(self):
-        self.day = "noinput"
-        self.time = "noinput"
-        self.name = "noinput"
-        self.url = "noinput"
+  def __init__(self):
+    self.day = "noinput"
+    self.time = "noinput"
+    self.name = "noinput"
+    self.url = "noinput"
 
-    def printData(self):
-        print(self.day, self.time, self.name, self.url)
+  def printData(self):
+    print(self.day, self.time, self.name, self.url)
 
-    def setDay(self, day):
-        self.day = day
+  def setDay(self, day):
+    self.day = day
 
-    def setTime(self, time):
-        self.time = time
+  def setTime(self, time):
+    self.time = time
 
-    def setName(self, name):
-        self.name = name
+  def setName(self, name):
+    self.name = name
 
-    def setUrl(self, url):
-        self.url = url
+  def setUrl(self, url):
+    self.url = url
 
 
 newData = dataset()
 
 
 def register():
-    flag = True
-    if comboboxDay.get() != "":
-        newData.setDay(comboboxDay.get())
-    else:
-        flag = False
-    if comboboxTimeHour.get() != "" and comboboxTimeMin != "":
-        newData.setTime(comboboxTimeHour.get() + comboboxTimeMin.get())
-    else:
-        flag = False
+  flag = True
+  if comboboxDay.get() != "":
+    newData.setDay(comboboxDay.get())
+  else:
+    flag = False
+  if comboboxTimeHour.get() != "" and comboboxTimeMin != "":
+    newData.setTime(comboboxTimeHour.get() + comboboxTimeMin.get())
+  else:
+    flag = False
 
-    if inputName.get() != "":
-        newData.setName(inputName.get())
-    else:
-        flag = False
+  if inputName.get() != "":
+    newData.setName(inputName.get())
+  else:
+    flag = False
 
-    if inputUrl.get() != "":
-        newData.setUrl(inputUrl.get())
-    else:
-        flag = False
-    if (flag):
-        newData.printData()
-        root.destroy()
+  if inputUrl.get() != "":
+    newData.setUrl(inputUrl.get())
+  else:
+    flag = False
+  if (flag):
+    newData.printData()
+    root.destroy()
 
-
-button = tkinter.Button(root, text="register", command=register)
-button.grid(column=0, row=9)
 
 labelDay = tkinter.Label(text="the Day (Sun, Mon, Tue, Wed, Thu, Fri, Sat)")
 labelDay.grid(column=0, row=0, padx=10, pady=5, sticky=tkinter.W)
@@ -68,27 +65,11 @@ labelTime = tkinter.Label(
 labelTime.grid(column=0, row=3, padx=10, pady=5, sticky=tkinter.W)
 # inputTime = tkinter.Entry(width=40)
 # inputTime.grid(column=0, row=4, sticky=tkinter.W)
-
-labelName = tkinter.Label(text="the meeting name")
-labelName.grid(column=0, row=5, padx=10, pady=5, sticky=tkinter.W)
-inputName = tkinter.Entry(width=40)
-inputName.grid(column=0, row=6, sticky=tkinter.W, padx=10)
-
-labelURL = tkinter.Label(text="the meeting URL")
-labelURL.grid(column=0, row=7, padx=10, pady=5, sticky=tkinter.W)
-inputUrl = tkinter.Entry(width=40)
-inputUrl.grid(column=0, row=8, sticky=tkinter.W, padx=10)
-
-
 moduleDay = ('Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat')
 moduleTimeHour = ('00', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10',
                   '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23')
 moduleTimeMin = ('00', '05', '10', '15', '20', '25',
                  '30', '35', '40', '45', '50', '55')
-
-# def select_combo(event):
-#   print(combobox.get())
-
 
 vDay = tkinter.StringVar()
 vTimeHour = tkinter.StringVar()
@@ -103,13 +84,26 @@ comboboxTimeMin = ttk.Combobox(
     root, textvariable=vTimeMin, values=moduleTimeMin, width=10, style="office.TCombobox")
 comboboxTimeMin.grid(column=0, row=4, padx=10)
 
+labelName = tkinter.Label(text="the meeting name")
+labelName.grid(column=0, row=5, padx=10, pady=5, sticky=tkinter.W)
+inputName = tkinter.Entry(width=40)
+inputName.grid(column=0, row=6, sticky=tkinter.W, padx=10)
+
+labelURL = tkinter.Label(text="the meeting URL")
+labelURL.grid(column=0, row=7, padx=10, pady=5, sticky=tkinter.W)
+inputUrl = tkinter.Entry(width=40)
+inputUrl.grid(column=0, row=8, sticky=tkinter.W, padx=10)
+
+button = tkinter.Button(root, text="register", command=register)
+button.grid(column=0, row=9)
+
 
 def quit():
-    root.destroy()
+  root.destroy()
 
 
-button = tkinter.Button(root, text="quit", command=quit)
-button.grid(column=0, row=9, sticky=tkinter.E)
+buttonQuit = tkinter.Button(root, text="quit", command=quit)
+buttonQuit.grid(column=0, row=9, sticky=tkinter.E)
 
 
 # root.grid_columnconfigure(0, weight=1)
--- src/del.py
+++ src/del.py
@@ -2,9 +2,9 @@ import tkinter
 import tkinter.ttk as ttk
 from typing import Counter
 
-
 root = tkinter.Tk()
 root.title("AUTOZOOM2 --delete URL--")
+root.minsize(width=600, height=400)
 
 tree = ttk.Treeview(root)
 
@@ -28,11 +28,11 @@ file = open(fname, encoding='utf-8')
 counter = 0
 data = []
 for row in file:
-    counter += 1
-    data.append(row)
-    row = row.split()
-    tree.insert("", "end", values=(
-        str(counter), row[0], row[1][0:2] + ":" + row[1][2:], row[2]))
+  counter += 1
+  data.append(row)
+  row = row.split()
+  tree.insert("", "end", values=(
+      str(counter), row[0], row[1][0:2] + ":" + row[1][2:], row[2]))
 file.close()
 
 tree.pack()
@@ -48,29 +48,29 @@ vbar1.pack(side=tkinter.LEFT, fill=tkinter.Y)
 vbar1.place(x=570, y=40, height=200)
 
 
-label = tkinter.Label(text="input the numbe you want to delete ->")
+label = tkinter.Label(text="input the number you want to delete ->")
 label.grid(column=0, row=9, padx=5, pady=5, sticky=tkinter.E)
 toDel = tkinter.Entry(width=40)
 toDel.grid(column=1, row=9, sticky=tkinter.W)
 
 
 def run():
-    global counter
-    if int(toDel.get()) and 1 <= int(toDel.get()) <= counter:
-        delnum = int(toDel.get())
-        counter = 0
-        for row in data:
-            counter += 1
-            if (counter == delnum):
-                continue
-            print(row)
-        root.destroy()
+  global counter
+  if int(toDel.get()) and 1 <= int(toDel.get()) <= counter:
+    delnum = int(toDel.get())
+    counter = 0
+    for row in data:
+      counter += 1
+      if (counter == delnum):
+        continue
+      print(row)
+    root.destroy()
 
 
 def quit():
-    for row in data:
-        print(row)
-    root.destroy()
+  for row in data:
+    print(row)
+  root.destroy()
 
 
 button = tkinter.Button(root, text="delete", command=run)
@@ -80,21 +80,21 @@ button = tkinter.Button(root, text="quit", command=quit)
 button.grid(column=3, row=9)
 
 
-root.grid_columnconfigure(0, weight=1)
-root.grid_columnconfigure(1, weight=1)
-root.grid_columnconfigure(2, weight=1)
-root.grid_columnconfigure(3, weight=1)
-root.grid_columnconfigure(4, weight=1)
+# root.grid_columnconfigure(0, weight=1)
+# root.grid_columnconfigure(1, weight=1)
+# root.grid_columnconfigure(2, weight=1)
+# root.grid_columnconfigure(3, weight=1)
+# root.grid_columnconfigure(4, weight=1)
 # root.grid_rowconfigure(0, weight=1)
-root.grid_rowconfigure(1, weight=1)
-root.grid_rowconfigure(2, weight=1)
-root.grid_rowconfigure(3, weight=1)
-root.grid_rowconfigure(4, weight=1)
-root.grid_rowconfigure(5, weight=1)
-root.grid_rowconfigure(6, weight=1)
-root.grid_rowconfigure(7, weight=1)
-root.grid_rowconfigure(8, weight=1)
-root.grid_rowconfigure(9, weight=1)
+# root.grid_rowconfigure(1, weight=1)
+# root.grid_rowconfigure(2, weight=1)
+# root.grid_rowconfigure(3, weight=1)
+# root.grid_rowconfigure(4, weight=1)
+# root.grid_rowconfigure(5, weight=1)
+# root.grid_rowconfigure(6, weight=1)
+# root.grid_rowconfigure(7, weight=1)
+# root.grid_rowconfigure(8, weight=1)
+# root.grid_rowconfigure(9, weight=1)
 
 
 root.protocol("WM_DELETE_WINDOW", quit)
