a4a7f8abadbd0cc7ed8d8896cbf52b6f9e462e47
kakeru macbook <kakekakemiya920@g.ecc.u-tokyo.ac.jp>
2021-10-30 11:05:59 +0900

    mod installer

--- src/test.py
+++
@@ -1,8 +0,0 @@
-# import os
-# path = "/Users/kakeru/autozoom2/src/autozoom2"
-# os.system(path)
-
-for i in range(23):
-  print('\'' + str(i) + '\',')  # , end=" ")
-for i in range(20):
-  print('\'' + str(i * 5) + '\',')  # , end=" ")
